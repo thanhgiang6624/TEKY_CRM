8e9a7f1709a215a2a2e84453e8e68e91cc160f2e
Doan Van Giang <thanhgiang6624@gmail.com>
2026-05-13 22:32:52 +0700

    update staticfiles storage backend in settings.py for improved performance

--- teky_crm/settings.py
+++ teky_crm/settings.py
@@ -159,7 +159,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
